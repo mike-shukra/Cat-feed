bc85bf6646fd7d2aacb20ef999505ebca306cd6a
Mikhail Shkuro <46484281+mike-shukra@users.noreply.github.com>
2019-07-03 18:22:56 +0300

    Update cat-feed-2.ino

--- cat-feed-2/cat-feed-2.ino
+++ cat-feed-2/cat-feed-2.ino
@@ -1,44 +1,105 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+
+#ifndef STASSID
+#define STASSID "******"
+#define STAPSK  "******"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
 #include <Servo.h> 
 #include <Wire.h>
 #include "RTClib.h"
 
 RTC_DS3231 rtc;
 
-//test
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 Servo servo1;         //объект сервопривод 
  
-const uint8_t servoPIN = 6;     //контакт сервопривода
-const uint8_t MOSFET_pin = 3;   // пин мосфета
-const int buttonPin = 2;     // номер входа, подключенный к кнопке
+#define servoPIN D1     //контакт сервопривода
+#define mosfetPIN D5   // пин мосфета
 
-//интервал проверки времени
-//30*60*1000
-uint32_t period_timing = 30*60*1000; 
 
-// время между открытиями в мс = 12ч*60мин*60сек*1000
-//2*60*60*1000
-uint32_t period_time = 7200000; 
-
-// переменная таймера, максимально большой целочисленный тип (он же uint32_t)
-uint32_t my_timer;
+uint32_t period_timing = 20000; //30*60*1000 // интервал между открытиями в мс = 12ч*60мин*60сек*1000
+uint32_t period_time = 1000; //3*60*60*1000 // интервал проверок
+uint32_t my_timer; // переменная таймера, максимально большой целочисленный тип (он же uint32_t)
 
 uint32_t my_timing; // Пауза
 
-uint32_t previousMillis = 0;
-int val = 0;
 
 
 const uint8_t Y0=0; //угол сервы в закрытом положении
-const uint32_t Y1=27; //угол сервы в открытом положении
+const uint32_t Y1=20; //угол сервы в открытом положении
 const uint32_t t=50; //приостановка в промежуточных положениях при открытии, мс (режим тряски)
 
 uint32_t hourNow;
 
 void setup() {
-  Serial.begin(9600); //test последовательный порт для отображения данных
-  delay(100); // wait for console opening
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  delay(1000); // wait for console opening
 
   if (! rtc.begin()) {
     Serial.println("Couldn't find RTC");
@@ -52,14 +113,12 @@ void setup() {
     // January 21, 2014 at 3am you would call:
     // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
   }
-  
-  pinMode(buttonPin, INPUT);  // инициализируем пин, подключенный к кнопке, как вход
-  
-  pinMode(MOSFET_pin, OUTPUT); // пин мосфета как выход
+
+  pinMode(servoPIN, OUTPUT); // пин сервы как выход
+  pinMode(mosfetPIN, OUTPUT); // пин мосфета как выход
 
   my_timer = millis();   // "сбросить" таймер
   my_timing = millis();   // "сбросить" таймер
-  previousMillis = millis();
   
   Serial.println("setup"); 
   DateTime now = rtc.now();
@@ -79,48 +138,41 @@ void setup() {
   Serial.print(now.second(), DEC);
   Serial.println(); 
 
-  Open();// Открытие крышки
+  Open() ;// Открытие крышки
   delay(100);
 
 }
  
 void loop() {
-   // Пауза
-   //Serial.println("Test");  
+  ArduinoOTA.handle();
 
-   if(digitalRead(buttonPin) == HIGH) { 
-       Serial.println("ButtonP");  
-       Open();
-     }
-
+  // Пауза
   
-  if (millis() - my_timing > period_timing){
+
+  if (millis() - my_timing > period_time){
     my_timing = millis();   // "сбросить" таймер
 
-    DateTime now = rtc.now();
-    hourNow = now.hour(), DEC;
-    //hourNow = 4;
+    //DateTime now = rtc.now();
+    //hourNow = now.hour(), DEC;
+    hourNow = 10;
     Serial.println(hourNow);
+     
 
-    if (hourNow > 6) {
-      
-      Serial.println(millis());
+    if (hourNow > 6 && millis() - my_timer > period_timing) {
+
+	  Serial.println(millis());
       Serial.println(my_timer);
-      Serial.println(period_time);
-      
-      if (millis() - my_timer > period_time) {
+
+      my_timer = millis();   // "сбросить" таймер
+      // набор функций, который хотим выполнить один раз за период
         
-        my_timer = millis();   // "сбросить" таймер
-        // набор функций, который хотим выполнить один раз за период
-        
-        Open() ;// Открытие крышки
-        delay(100);
-      }
+      Open() ;// Открытие крышки
+      delay(1000);
     }
   }
  
 
-  delay(100);
+  delay(1000);
 
 
 } //loop
@@ -133,7 +185,7 @@ void Open() {
 
 // открыть мосфет
   Serial.println("open MOSFET");  
-  digitalWrite(MOSFET_pin, 1);    // подаём сигнал на пин мосфета
+  digitalWrite(mosfetPIN, 1);    // подаём сигнал на пин мосфета
   delay(100);
 
   servo1.attach(servoPIN); // подкючаем сервопривод
@@ -179,7 +231,7 @@ void Open() {
   Serial.println("function stop");  
 
 // закрыть мосфет
-  digitalWrite(MOSFET_pin, 0);    // подаём сигнал на пин мосфета
+  digitalWrite(mosfetPIN, 0);    // подаём сигнал на пин мосфета
   Serial.println("close MOSFET");  
   delay(100);  
 }
